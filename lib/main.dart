50fe785fa5168a3a96c10f68e64208dd0bcd67f5
Sardor Ergashaliyev <sardorergashaliyev@gmail.com>
2022-11-09 19:52:43 +0500

    Shadow part 4

--- lib/main.dart
+++ lib/main.dart
@@ -10,42 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Container(
-            color: const Color.fromARGB(255, 255, 255, 255),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    height: 80,
-                    width: 80,
-                    decoration: const BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              color: Colors.orange,
-                              spreadRadius: 0,
-                              offset: Offset(10, 10))
-                        ]),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.orange,
-                              blurRadius: 10,
-                              spreadRadius: 2,
-                              offset: Offset(5, 10))
-                        ]),
-                  ),
+      home: Stack(
+        children: [
+          Positioned(
+              top: 250,
+              child: Container(
+                height: 380,
+                width: 380,
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                        offset: Offset(-2, 5))
+                  ],
+                  color: Color.fromARGB(255, 0, 255, 170),
+                  shape: BoxShape.circle,
+                ),
+              )),
+          Positioned(
+            top: 290,
+            right: 50,
+            child: Container(
+              height: 300,
+              width: 300,
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 2,
+                      spreadRadius: 2,
+                      offset: Offset(-2, 5))
                 ],
+                color: Color.fromARGB(255, 0, 255, 170),
+                // color: Color.fromARGB(255, 0, 255, 170),
+                shape: BoxShape.circle,
               ),
-            )));
+            ),
+          ),
+          Positioned(
+            top: 330,
+            right: 90,
+            child: Container(
+              height: 220,
+              width: 220,
+              decoration: const BoxDecoration(
+                // color: Color.fromARGB(255, 0, 255, 170),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 2,
+                      spreadRadius: 2,
+                      offset: Offset(-2, 5))
+                ],
+                color: Color.fromARGB(255, 0, 255, 170),
+                shape: BoxShape.circle,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
